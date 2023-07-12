f417bdf517e6b2e19fd8ac167c69d14f7555a511
JuditRoca <roca.tovar@gmail.com>
2023-07-12 16:55:24 +0200

    act app

--- src/app.py
+++ src/app.py
@@ -3,6 +3,7 @@ import os
 import pickle
 import pandas as pd
 import sqlite3
+import openai
 
 os.chdir(os.path.dirname(__file__))
 
@@ -12,3 +13,22 @@ app.config['DEBUG'] = True
 @app.route("/", methods=['GET'])
 def hello():
     return render_template('index.html')
+
+@app.route('/', methods=['POST'])
+def my_form_post():
+    try:
+        api_key = request.form['api_key']
+        openai.api_key = api_key
+        #openai.api_key = open(api_key_path, "r").read()
+        variable = request.form['variable']
+        response = openai.Completion.create(engine = "text-davinci-003",
+                                        prompt = variable + "Respuesta corta, por favor",
+                                        max_tokens=1000, temperature=0.8)
+        
+
+    
+    except:
+        return render_template('index_3.html')
+    
+    
+    app.run(host="0.0.0.0", port=5000, debug=True)
