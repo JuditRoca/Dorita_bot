46660f4dc38998cf75051549c3179e138d5568c1
Giacomo <giamosalerno@gmail.com>
2023-07-13 16:09:27 +0200

    Aquí no ha pasao nada.

--- src/app.py
+++ src/app.py
@@ -1,34 +1,61 @@
+from datetime import datetime
 from flask import Flask, render_template, request
+from langchain.agents import load_tools
+from langchain.agents import initialize_agent
+from langchain.llms import OpenAI
 import os
-import pickle
 import pandas as pd
 import sqlite3
-import openai
 
 os.chdir(os.path.dirname(__file__))
+os.environ["OPENAI_API_KEY"] = "xxxx"
+os.environ["SERPAPI_API_KEY"] = "xxx"
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-@app.route("/", methods=['GET'])
-def hello():
-    return render_template('index.html')
+historial = []
+bbdd = 'data/bbdd.bd'
 
-@app.route('/', methods=['POST'])
-def my_form_post():
+@app.route('/', methods=['GET', 'POST'])
+def home(pregunta:str):
+    """
+    Basic function where the user asks something and returns the answer from ChatGPT. It will also save it in the Cloud DDBB, in this case, AWS.
+
+    Parameters 
+    ----------
+    Type in the interface what you want to ask and it will return the answer.
+    """
     try:
-        api_key = request.form['api_key']
-        openai.api_key = api_key
-        #openai.api_key = open(api_key_path, "r").read()
-        variable = request.form['variable']
-        response = openai.Completion.create(engine = "text-davinci-003",
-                                        prompt = variable + "Respuesta corta, por favor",
-                                        max_tokens=1000, temperature=0.8)
         
+        if request.method == 'GET':
+            return render_template('index.html')
+        
+        if request.form['question']:
+            question = request.form['question']
+            historial.append(question)
+        
+            # Load the model
+            llm = OpenAI()
 
-    
+            # Load in some tools to use
+            tools = load_tools(["serpapi", "llm-math"], llm=llm)
+
+            # Finally, let's initialize an agent with:
+            # 1. The tools
+            # 2. The language model
+            # 3. The type of agent we want to use.
+
+            agent = initialize_agent(tools, llm, agent="zero-shot-react-description", verbose=True)
+
+
+            # Now let's test it out!
+            respuesta = agent.run(question)
+
+            historial.append(respuesta)
+            
+
     except:
         return render_template('index_3.html')
     
-    
-    app.run(host="0.0.0.0", port=5000, debug=True)
+app.run(host="0.0.0.0", port=5000, debug=True)
